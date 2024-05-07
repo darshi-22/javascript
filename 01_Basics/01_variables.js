5851ab458ad5e3c3657da35a2544ad1eedf43b81
Darshi Shah <149527523+darshi-22@users.noreply.github.com>
2024-05-07 13:19:35 +0000

    1st commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID=12345
+let accountEmail="abc@gmail.com"
+var accountName="Darshi"
+accountCity="Jaipur"
+
+console.log(accountEmail)
+console.table([accountID,accountEmail,accountName,accountCity])
